cfee01733c4acdedf72005c154db8d764ac51062
cedi <c_edi@gmx.de>
2014-06-09 14:37:24 +0200

    commit

--- app/src/main/java/com/c3d1/rememberphotosettings/app/database/DatabaseHelper.java
+++ app/src/main/java/com/c3d1/rememberphotosettings/app/database/DatabaseHelper.java
@@ -7,16 +7,16 @@ import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper
 {
-    public static final String	DATABASE_NAME			= "PhotoSettings";
-    private static final int	DATABASE_VERSION		= 3;
-    public static final String	TABLE_PHOTO_NAME		= "Photos";
-    public static final String	COL_PHOTO_ID			= "photos_id";
+	public static final String	DATABASE_NAME			= "PhotoSettings";
+	private static final int	DATABASE_VERSION		= 3;
+	public static final String	TABLE_PHOTO_NAME		= "Photos";
+	public static final String	COL_PHOTO_ID			= "photos_id";
 	public static final String	COL_PHOTO_TITLE			= "Title";
-    public static final String	COL_PHOTO_FILMID		= "Film_id";
-    public static final String	COL_PHOTO_APERTURE		= "Aperture";
-    public static final String	COL_PHOTO_SHUTTER		= "Shutter";
-    public static final String	COL_PHOTO_MONOCHROM		= "Monochrom";
-    public static final String	COL_PHOTO_LENS		    = "Lens";
+	public static final String	COL_PHOTO_FILMID		= "Film_id";
+	public static final String	COL_PHOTO_APERTURE		= "Aperture";
+	public static final String	COL_PHOTO_SHUTTER		= "Shutter";
+	public static final String	COL_PHOTO_MONOCHROM		= "Monochrom";
+	public static final String	COL_PHOTO_LENS		    = "Lens";
 	public static final String	COL_PHOTO_DESCRIPTION   = "Description";
 
 	public static final String	TABLE_FILM_NAME			= "Film";
@@ -27,17 +27,17 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	public static final String	COL_FILM_DESCRIPTION	= "Description";
 	public static final String	COL_FILM_SW				= "SW";
 
-    private static final String	DATABASE_PHOTO_CREATE	=
-            "CREATE TABLE " + TABLE_PHOTO_NAME + " ( "
-            + COL_PHOTO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+	private static final String	DATABASE_PHOTO_CREATE	=
+			"CREATE TABLE " + TABLE_PHOTO_NAME + " ( "
+			+ COL_PHOTO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
 			+ COL_PHOTO_TITLE + " TEXT NOT NULL,  "
-            + COL_PHOTO_FILMID + " INTEGER NOT NULL, "
-            + COL_PHOTO_APERTURE + " TEXT NOT NULL, "
-            + COL_PHOTO_SHUTTER + " TEXT NOT NULL, "
-            + COL_PHOTO_MONOCHROM + " INTEGER, "
-            + COL_PHOTO_LENS + " TEXT, "
+			+ COL_PHOTO_FILMID + " INTEGER NOT NULL, "
+			+ COL_PHOTO_APERTURE + " TEXT NOT NULL, "
+			+ COL_PHOTO_SHUTTER + " TEXT NOT NULL, "
+			+ COL_PHOTO_MONOCHROM + " INTEGER, "
+			+ COL_PHOTO_LENS + " TEXT, "
 			+ COL_PHOTO_DESCRIPTION + " TEXT "
-            + ")";
+			+ ")";
 
 	private static final String DATABASE_FILM_CREATE =
 			"CREATE TABLE " + TABLE_FILM_NAME + " ( "
@@ -49,28 +49,28 @@ public class DatabaseHelper extends SQLiteOpenHelper
 			+ COL_FILM_SW + "INTEGER "
 			+ " ) ";
 
-    private Context				mMyContext;
+	private Context				mMyContext;
 
-    public DatabaseHelper( Context context )
-    {
-        super( context, DATABASE_NAME, null, DATABASE_VERSION );
-        mMyContext = context;
-    }
+	public DatabaseHelper( Context context )
+	{
+		super( context, DATABASE_NAME, null, DATABASE_VERSION );
+		mMyContext = context;
+	}
 
-    @Override
-    public void onCreate( SQLiteDatabase db )
-    {
-        db.execSQL( DATABASE_PHOTO_CREATE );
+	@Override
+	public void onCreate( SQLiteDatabase db )
+	{
+		db.execSQL( DATABASE_PHOTO_CREATE );
 		db.execSQL( DATABASE_FILM_CREATE );
-    }
+	}
 
-    @Override
-    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion )
-    {
-        Log.w( SQLiteOpenHelper.class.getName(), "Upgraiding database " + DATABASE_NAME + "from Version "
-                + oldVersion + " to " + newVersion + ". Old Data will be destroyed" );
+	@Override
+	public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion )
+	{
+		Log.w( SQLiteOpenHelper.class.getName(), "Upgraiding database " + DATABASE_NAME + "from Version "
+												 + oldVersion + " to " + newVersion + ". Old Data will be destroyed" );
 
-        db.execSQL( "DROP TABLE IF EXISTS " + TABLE_PHOTO_NAME );
-        onCreate( db );
-    }
+		db.execSQL( "DROP TABLE IF EXISTS " + TABLE_PHOTO_NAME );
+		onCreate( db );
+	}
 }
